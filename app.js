c0ab6be9acacdf2959520c226abca2364036767d
Tanushree <60546683+tanuuu18@users.noreply.github.com>
2023-06-29 14:52:59 +0000

    create request for delete the book

--- app.js
+++ app.js
@@ -51,3 +51,20 @@ app.post("/books", (req, res) => {
       });
   });
   
+
+  //delete the book
+  app.delete("/books/:id", (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+      db.collection("books")
+        .deleteOne({ _id: new ObjectId(req.params.id) })
+        .then((result) => {
+          res.status(200).json(result);
+        })
+        .catch((err) => {
+          res.status(500).json({ error: "Could not delete document" });
+        });
+    } else {
+      res.status(500).json({ error: "Could not delete document" });
+    }
+  });
+  
